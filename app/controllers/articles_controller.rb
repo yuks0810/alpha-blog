aa8b83f1ce7db8ea3c6baa0d37fb737535eff8a3
Yuki <dorcushopeino1@gmail.com>
2019-06-23 03:45:14 +0000

    既存の記事の編集を可能に

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -13,6 +17,17 @@ class ArticlesController < ApplicationController
     end
   end
   # create action does not need a template as def new does
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   
   def show 
     @article = Article.find(params[:id])
