c612a585c7d45676cb5a0c00f3ab5919b99e459d
Yuki <dorcushopeino1@gmail.com>
2019-06-26 16:59:03 +0000

    add edit update functonality and partial of users folda

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,26 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      flash[:success] = "Welcome to the Yuki blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "アカウント情報が変更されました。"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  # ここでusersからのroutesをコピーしている
 end
