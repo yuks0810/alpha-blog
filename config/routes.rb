af6daf38f659a8a3590c053e69c23f1af098d982
Yuki <dorcushopeino1@gmail.com>
2019-06-29 03:22:22 +0000

    add login and logout (create and destroy)

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,11 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   # ここでusersからのroutesをコピーしている
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
+
 end
