6be4a0427a6568d7b088e70460c46b9a6d38ff7b
Yuki <dorcushopeino1@gmail.com>
2019-06-29 05:22:14 +0000

    restric actions from UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update, :show]
+  
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end 
@@ -19,11 +20,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "アカウント情報が変更されました。"
       redirect_to articles_path
@@ -33,8 +33,11 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
   private
